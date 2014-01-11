7f3b40c40a5f483e0d5d1b3f30086fd8a155f667
lzuk <lukasz.zuk2@gmail.com>
2014-01-11 17:50:49 +0100

    Wprowadzona mapa
    Signed-off-by: lzuk <lukasz.zuk2@gmail.com>

--- WifiMapCreator/src/main/java/com/lzuk/mapcreator/DummySectionFragmentMap.java
+++ WifiMapCreator/src/main/java/com/lzuk/mapcreator/DummySectionFragmentMap.java
@@ -7,6 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Krzysiek on 27.12.13.
  */
@@ -14,6 +24,9 @@ public class DummySectionFragmentMap extends Fragment {
 
     public static final String ARG_SECTION_NUMBER = "section_number";
     private FragmentActivity fragmentActivity;
+    private GoogleMap map;
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
 
     public DummySectionFragmentMap(FragmentActivity fragmentActivity) {
         this.fragmentActivity=fragmentActivity;
@@ -22,7 +35,20 @@ public class DummySectionFragmentMap extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_main_map, container, false);
+        setMap();
 
         return rootView;
+    }
+
+    private void setMap(){
+        map = ((SupportMapFragment) fragmentActivity.getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        map.setBuildingsEnabled(true);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(HAMBURG));
+        //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
     }
 }
